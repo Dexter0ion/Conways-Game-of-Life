311d3c4a49ce2a3f3202edfa20aaefd3940cfef3
Yongsheng Xu <chuxdesign@hotmail.com>
2017-06-03 13:00:07 +0800

    完成getNextState 方法

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-// todo 游戏逻辑
+import { countAlive, getNextStateWhenAlive, getNextStateWhenDead } from './js/gameLogic'
+
+export function getNextState (grids) {
+    if (grids.length === 0) return;
+    const rows = grids.length;
+    const cols = grids[0].length;
+
+    // 返回新的场景
+    let retArr = new Array(rows);
+    for (let row = 0; row < rows; ++row) {
+        retArr[row] = new Array(cols);
+    }
+
+    // 扩展场景
+    let extendedGrids = new Array(rows);
+    for (let i = 0; i < rows; ++i) {
+        extendedGrids[i] = [0, ...grids[i], 0];
+    }
+    extendedGrids = [new Array(cols + 2, 0), ...extendedGrids, new Array(cols + 2, 0)];
+
+    // 判断 储存下一个状态
+    for (let i = 1; i <= rows; ++i) {
+        for (let j = 1; j <= cols; ++j) {
+            const aliveNums = countAlive([i, j], extendedGrids);
+            if (extendedGrids[i][j] === 1) {
+                retArr[i - 1][j - 1] = getNextStateWhenAlive(aliveNums);
+            } else {
+                retArr[i - 1][j - 1] = getNextStateWhenDead(aliveNums);
+            }
+        }
+    }
+    return retArr;
+}
