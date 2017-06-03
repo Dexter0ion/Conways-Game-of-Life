62c542402c97023b07e80941efd694c863b58008
Yongsheng Xu <chuxdesign@hotmail.com>
2017-06-03 10:49:02 +0800

    添加第一个方法的测试,并初始化对应方法

--- src/js/gameLogic.js
+++ src/js/gameLogic.js
@@ -1,3 +1,8 @@
 /**
- * Created by xuyongsheng on 2017/6/3.
+ * 输入目标点坐标及整个场景，返回目标周围状态为存活的细胞数量
+ * @param indexArr 目标点坐标
+ * @param grids 整个场景
  */
+export function countAlive (indexArr, grids) {
+
+}
--- test/findNeightbor.spec.js
+++ test/findNeightbor.spec.js
@@ -1,3 +1,59 @@
-/**
- * Created by xuyongsheng on 2017/6/3.
- */
+import { expect } from 'chai';
+import  { countAlive } from '../src/js/gameLogic';
+
+const mockOneNeightborZero = [
+    0, 0, 0,
+    0, 1, 0,
+    0, 0, 0
+];
+const mockOneNeightborTwo = [
+    0, 1, 0,
+    0, 1, 1,
+    0, 0, 0
+];
+const mockOneNeightborThree = [
+    0, 1, 0,
+    0, 1, 0,
+    1, 0, 1
+];
+const mockOneNeightborFour = [
+    1, 0, 0,
+    1, 1, 0,
+    1, 1, 0
+];
+const mockZeroNeightborThree = [
+    0, 0, 0,
+    1, 0, 0,
+    1, 1, 0
+];
+const mockZeroNeightborFour = [
+    1, 0, 0,
+    1, 0, 0,
+    1, 1, 0
+];
+
+describe('Test game logic', () => {
+    it('should return number 0 of mockOneNeightborZero', () => {
+        expect(countAlive([1, 1], mockOneNeightborZero)).equal(0);
+    });
+
+    it('should return number 2 of mockOneNeightborTwo', () => {
+        expect(countAlive([1, 1], mockOneNeightborTwo)).equal(2);
+    });
+
+    it('should return number 3 of mockOneNeightborThree', () => {
+        expect(countAlive([1, 1], mockOneNeightborThree)).equal(3);
+    });
+
+    it('should return number 4 of mockOneNeightborFour', () => {
+        expect(countAlive([1, 1], mockOneNeightborFour)).equal(4);
+    });
+
+    it('should return number 3 of mockZeroNeightborThree', () => {
+        expect(countAlive([1, 1], mockZeroNeightborThree)).equal(3);
+    });
+
+    it('should return number 3 of mockZeroNeightborFour', () => {
+        expect(countAlive([1, 1], mockZeroNeightborFour)).equal(4);
+    });
+});
