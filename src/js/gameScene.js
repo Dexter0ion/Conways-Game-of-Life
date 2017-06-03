467c42a4dc51f01473437462ac264803ab83b142
Yongsheng Xu <chuxdesign@hotmail.com>
2017-06-03 15:46:40 +0800

    - 重构帧率滑块代码 - 添加点击设置

--- src/js/gameScene.js
+++ src/js/gameScene.js
@@ -9,18 +9,15 @@ const columns = WIDTH / GRID;
 const rows = HEIGHT / GRID;
 
 let board, cnv, slider, sliderLabel;
+
+let cnvMarginLeft, cnvMarginTop;
+
 // 游戏可视化部分
 export function setup () {
     cnv = createCanvas(WIDTH, HEIGHT);
 
-    slider = createSlider(1, 60, 30, 1);
-    sliderLabel = createP('frame rate 0 fps');
-    sliderLabel.position(50, 600);
-    sliderLabel.style("margin-top", "-2px");
-    slider.position(200, 600);
-    slider.style('width', '80px');
-
     centerCanvas();
+    createFrameRateController();
 
     board = new Array(rows);
     for (let row = 0; row < rows; ++row) {
@@ -32,9 +29,18 @@ export function setup () {
 }
 
 function centerCanvas () {
-    const x = (windowWidth - width) / 2;
-    const y = (windowHeight - height) / 2;
-    cnv.position(x, y);
+    cnvMarginLeft = (windowWidth - width) / 2;
+    cnvMarginTop = (windowHeight - height) / 2;
+    cnv.position(cnvMarginLeft, cnvMarginTop);
+}
+
+function createFrameRateController () {
+    slider = createSlider(1, 60, 30, 1);
+    sliderLabel = createP('frame rate 0 fps');
+    sliderLabel.position(50, 600);
+    sliderLabel.style("margin-top", "-2px");
+    slider.position(200, 600);
+    slider.style('width', '80px');
 }
 
 export function draw () {
@@ -48,7 +54,9 @@ export function draw () {
             rect(j * GRID, i * GRID, GRID - 1, GRID - 1);
         }
     }
-    board = getNextState(board);
+    if (!paused) {
+        board = getNextState(board);
+    }
     const fr = slider.value();
     frameRate(fr);
     sliderLabel.html(`frame rate: ${paused ? 0 : fr} fps`);
@@ -77,7 +85,15 @@ function handleLoop () {
     }
 }
 
-export const mouseClicked = handleLoop;
+export function mouseClicked () {
+    const i = floor(mouseY / 20);
+    const j = floor(mouseX / 20);
+    if (j >= columns || i >= rows || i < 0 || j < 0 || !paused) {
+        return;
+    }
+    board[i][j] = board[i][j] === 1 ? 0 : 1;
+    redraw();
+}
 
 export function keyPressed () {
     if (keyCode == 32) {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { setup, draw, keyPressed } from './js/gameScene';
+import { setup, draw, keyPressed, mouseClicked } from './js/gameScene';
 
 if (window) {
     window.setup = setup;
     window.draw = draw;
     window.keyPressed = keyPressed;
+    window.mouseClicked = mouseClicked;
 }
 
